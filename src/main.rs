36f555acb81fcd5a87ddfedf849447978099a309
lsioctl <lsioctl@gmail.com>
2026-06-19 17:28:23 +0200

    chore: first ray, wip

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,87 @@
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
+use crate::ray::Ray;
+use crate::vector3::Vector3;
+
 mod color;
+mod ray;
 mod vector3;
 
 fn main() {
-    const IMG_WIDTH: i32 = 256;
-    const IMG_HEIGHT: i32 = 256;
-
     const IMG_PATH: &str = "./images/img_001.ppm";
 
     let f = File::create(IMG_PATH).expect("unable to create file");
 
     let mut fd = BufWriter::new(f);
 
+    let aspect_ratio = 16 as f64 / 9 as f64;
+    let image_width = 400 as i32;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    let image_height_raw = (image_width as f64 / aspect_ratio) as i32;
+    let image_height = match image_height_raw < 1 {
+        true => 1,
+        false => image_height_raw,
+    };
+
+    // Camera
+    // Viewport widths less than one are ok since they are real valued.
+    let viewport_height = 2 as f64;
+    let viewport_width = viewport_height * image_width as f64 / image_height as f64;
+    let focal_length = 1 as f64;
+    let camera_center = Vector3 {
+        x: 0 as f64,
+        y: 0 as f64,
+        z: 0 as f64,
+    };
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    let viewport_u = Vector3 {
+        x: viewport_width,
+        y: 0 as f64,
+        z: 0 as f64,
+    };
+    let viewport_v = Vector3 {
+        x: 0 as f64,
+        y: -viewport_height,
+        z: 0 as f64,
+    };
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    // Calculate the location of the upper left pixel.
+    let viewport_upper_left = camera_center
+        - Vector3 {
+            x: 0 as f64,
+            y: 0 as f64,
+            z: focal_length,
+        }
+        - viewport_u / 2 as f64
+        - viewport_v / 2 as f64;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
     writeln!(fd, "P3").expect("unable to write line");
-    writeln!(fd, "{}", IMG_WIDTH).expect("unable to write line");
-    writeln!(fd, "{}", IMG_HEIGHT).expect("unable to write line");
+    writeln!(fd, "{}", image_width).expect("unable to write line");
+    writeln!(fd, "{}", image_height).expect("unable to write line");
     writeln!(fd, "255").expect("unable to write line");
 
-    for line in 0..IMG_HEIGHT {
-        println!("Scanlines remaining: {}", IMG_HEIGHT - line);
-        for col in 0..IMG_WIDTH {
+    for line in 0..image_height {
+        println!("Scanlines remaining: {}", image_height - line);
+        for col in 0..image_width {
+            let pixel_center =
+                pixel00_loc + (line as f64 * pixel_delta_u) + (col as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray {
+                origin: camera_center,
+                direction: ray_direction,
+            };
+
             let pixel_color = color::Color {
-                x: col as f64 / (IMG_WIDTH - 1) as f64,
-                y: line as f64 / (IMG_HEIGHT - 1) as f64,
+                x: 0 as f64,
+                y: 0 as f64,
                 z: 0 as f64,
             };
 
---
+++ src/ray.rs
@@ -0,0 +1,12 @@
+use crate::vector3::Vector3;
+
+pub struct Ray {
+    pub origin: Vector3,
+    pub direction: Vector3,
+}
+
+impl Ray {
+    fn at(&self, time: f64) -> Vector3 {
+        self.origin + time * self.direction
+    }
+}
